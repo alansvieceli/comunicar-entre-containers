22a23d8e28c01ae3f8940cf50de1be5a4eb86dfa
Alan Vieceli <alan.vieceli@softplan.com.br>
2019-12-19 11:59:33 -0300

    Comunicaçao entre containers

---
+++ Servico01/Controllers/Servico01Controller.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Servico01.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class Servico01Controller : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "A", "B", "C", "D", "E", "F", "G", "H", "I", "J"
+        };
+
+        private readonly ILogger<Servico01Controller> _logger;
+
+        public Servico01Controller(ILogger<Servico01Controller> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Index()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Summary = Summaries[rng.Next(Summaries.Length)],
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55)                
+            })
+            .ToArray();
+        }
+
+        [HttpGet("Integracao")]
+        public async Task<IActionResult> Integracao()
+        {
+            using var client = new HttpClient();
+            HttpResponseMessage response = await client.GetAsync("http://servico02/servico02");
+            response.EnsureSuccessStatusCode();
+            string responseBody = await response.Content.ReadAsStringAsync();
+
+            return Content(responseBody, "application/json", Encoding.UTF8); 
+            //return Json(responseBody, JsonRequestBehavior.AllowGet);
+
+        }
+    }
+}
---
+++ Servico02/Controllers/Servico02Controller.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Servico02.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class Servico02Controller : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "1", "2", "2", "4", "5", "6", "7", "8", "9", "10"
+        };
+
+        private readonly ILogger<Servico02Controller> _logger;
+
+        public Servico02Controller(ILogger<Servico02Controller> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Summary = Summaries[rng.Next(Summaries.Length)],
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55)                
+            })
+            .ToArray();
+        }
+    }
+}
